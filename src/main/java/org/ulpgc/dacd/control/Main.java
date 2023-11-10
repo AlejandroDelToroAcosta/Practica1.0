80ba36c6dc341bc69a2aecd24106044411db6607
Alejandro Del Toro <aadeltoroo@gmail.com>
2023-11-10 16:06:15 +0000

    weatherController improved(execute() method created)

--- src/main/java/org/ulpgc/dacd/control/Main.java
+++ src/main/java/org/ulpgc/dacd/control/Main.java
@@ -1,20 +1,9 @@
 package org.ulpgc.dacd.control;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import org.jsoup.Jsoup;
-import org.ulpgc.dacd.model.Weather;
-
-import java.io.IOException;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        WeatherController weatherController = new WeatherController(new WeatherMapProvider());
+        weatherController.execute();
     }
 }
--- src/main/java/org/ulpgc/dacd/control/WeatherController.java
+++ src/main/java/org/ulpgc/dacd/control/WeatherController.java
@@ -3,21 +3,21 @@ package org.ulpgc.dacd.control;
 import org.ulpgc.dacd.model.Location;
 import org.ulpgc.dacd.model.Weather;
 
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherController {
-    public ArrayList<Location> locationArrayList;
-    public ArrayList<Instant> instantArrayList;
 
-    public WeatherController(ArrayList<Location> locationArrayList, ArrayList<Instant> instantArrayList) {
-        this.locationArrayList = locationArrayList;
-        this.instantArrayList = instantArrayList;
+    public WeatherProvider weatherProvider;
+
+    public WeatherController(WeatherProvider weatherProvider) {
+        this.weatherProvider = weatherProvider;
     }
 
-    public static void main(String[] args) {
+    public void execute() {
+
         Location elHierro = new Location(-16.25462, 28.46824, "Valverde");
         Location laPalma = new Location(-17.76421, 28.68351, " Santa Cruz de La Palma");
         Location laGomera = new Location(-17.11331, 28.09163, "San Sebastián de La Gomera");
@@ -30,29 +30,45 @@ public class WeatherController {
         List<Location> locationList = List.of(elHierro, laPalma,laGomera,tenerife,granCanaria,
                 fuerteventura,lanzarote, laGraciosa);
 
+        ArrayList<Instant> instantList = new ArrayList<>();
+        ArrayList<Weather> weatherArrayList = new ArrayList<>();
 
-        Instant instant = Instant.parse("2023-11-10T12:00:00Z");
-        Instant instant2 = Instant.parse("2023-11-11T12:00:00Z");
-        Instant instant3 = Instant.parse("2023-11-12T12:00:00Z");
-        Instant instant4 = Instant.parse("2023-11-13T12:00:00Z");
-        Instant instant5 = Instant.parse("2023-11-14T12:00:00Z");
-        List<Instant> instantList = List.of(instant, instant2, instant3, instant4, instant5);
+        createInstant(instantList);
+        getWeatherCall(instantList, locationList, weatherArrayList);
 
-        WeatherMapProvider weatherMapProvider = new WeatherMapProvider();
-
+    }
+    public static ArrayList<Instant> createInstant(ArrayList<Instant> instants ){
+        for (int i = 0; i < 5; i++) {
+            LocalDate hoy = LocalDate.now();
+            LocalTime hour = LocalTime.of(12, 0);
+            LocalDateTime todayHour = LocalDateTime.of(hoy, hour);
+            Instant instant = todayHour.toInstant(ZoneOffset.UTC);
+            Instant instant1 = instant.plus(i, ChronoUnit.DAYS);
+            instants.add(instant1);
+        }
+        return instants;
+    }
+    public static ArrayList<Weather> getWeatherCall(ArrayList<Instant> instantList, List<Location> locationList,
+                                                    ArrayList<Weather> weatherArrayList) {
+        WeatherProvider weatherProvider = new WeatherMapProvider();
 
         for (Location iteredLocation : locationList) {
             for (Instant iteredInstant : instantList) {
-                Weather weather = weatherMapProvider.getWeather(iteredLocation, iteredInstant);
+                Weather weather = weatherProvider.getWeather(iteredLocation, iteredInstant);
                 if (weather != null) {
-                    System.out.println("Weather for " + iteredLocation.getName() + " at " + instant + ":");
+                    System.out.println("Weather for " + iteredLocation.getName() + " at " + iteredInstant + ":");
                     System.out.println(weather);
                     System.out.println("\n");
                 } else {
                     System.out.println("No weather data found for " + iteredLocation.getName() + " at " + iteredInstant);
                 }
+                weatherArrayList.add(weather);
             }
         }
-
+        return weatherArrayList;
+    }
+    public static void main(String[] args) {
+        WeatherController weatherController = new WeatherController(new WeatherMapProvider());
+        weatherController.execute();
     }
 }
